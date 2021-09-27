5360c3e3d98a7270737114cff5442168809e1ca5
TorroSoft <torrozapata@gmail.com>
2021-09-27 14:43:22 -0700

    The state of cantidad input was created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React,{Fragment} from 'react';
+import React,{Fragment, useState} from 'react';
 import Header from './components/Header';
+import Formulario from './components/Formulario';
 function App() {
+     //Definiendo el state
+     const [cantidad, guardarCantidad]=useState(0);
+     
     return ( 
     <Fragment>
         <Header
         titulo = "cotizador Prestamos"
         />
+        <div className= "container">
+            <Formulario
+            cantidad = {cantidad}
+            guardarCantidad = {guardarCantidad}
+            />
+        </div>
     </Fragment>
     );
 }
